da6aa697c8949550eb586a0f51c31e058db661e9
mmk72 <mmkrishna.p@gmail.com>
2020-09-20 00:47:09 +0530

    removed twilio from views

--- website/views.py
+++ website/views.py
@@ -1,6 +1,5 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.models import User, auth
-from twilio.rest import Client
 from .models import Person
 
 # Create your views here.
