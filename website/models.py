0b57f3812efb59a35d3d0e76d48fef9e04785e80
mmk72 <mmkrishna.p@gmail.com>
2020-09-24 17:58:02 +0530

    tweaked for dynamic pics

--- website/models.py
+++ website/models.py
@@ -1,10 +1,18 @@
 from django.db import models
+from django.db.models.signals import pre_delete
+from django.dispatch.dispatcher import receiver
 
 # Create your models here.
 class Person(models.Model):
     first_name = models.CharField(max_length=30)
     last_name = models.CharField(max_length=20)
-    address = models.TextField()
+    img = models.ImageField(upload_to='images')
 
     def __str__(self):
         return self.first_name
+
+@receiver(pre_delete, sender=Person)
+def delete_img(sender, instance, **kwargs):
+    # Pass false so FileField doesn't save the model.
+    if instance.img:
+        instance.img.delete(False)
